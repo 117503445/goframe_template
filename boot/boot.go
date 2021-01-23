cbed064b4333f92a91fe04021badb6bc8290360f
117503445 <t117503445@gmail.com>
2021-01-23 10:51:09 +0800

    feat: judge database exist
    1.0.1

--- boot/boot.go
+++ boot/boot.go
@@ -12,12 +12,12 @@ import (
 //InitDatabase Create database if not exists
 func InitDatabase() {
 	link := g.Cfg().Get("database.link").(string)
-	// g.Log().Line().Info(link)
+	// g.Log().Line().Debug(link)
 
 	linkWithoutDbName := strings.Join(strings.Split(strings.Split(link, "/")[0], ":")[:][1:], ":") + "/"
 	dbName := strings.Split(link, "/")[1]
 
-	// g.Log().Line().Info(linkWithoutDbName)
+	// g.Log().Line().Debug(linkWithoutDbName)
 
 	sqlDB, err := sql.Open("mysql", linkWithoutDbName)
 	if err != nil {
@@ -25,10 +25,28 @@ func InitDatabase() {
 	}
 	defer sqlDB.Close()
 
-	_, err = sqlDB.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %v", dbName))
+	query := fmt.Sprintf("SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='%v'", dbName)
+	// g.Log().Debug(query)
+
+	result, err := sqlDB.Query(query)
 	if err != nil {
 		g.Log().Line().Panic(err)
 	}
+
+	isDbExists := false
+	for result.Next() {
+		isDbExists = true // 只要查到了一条记录,就说明数据库存在
+		break
+	}
+	// g.Log().Debug(isDbExists)
+	if !isDbExists {
+		g.Log().Info(fmt.Sprintf("CREATE DATABASE %v", dbName))
+		_, err = sqlDB.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %v", dbName))
+		if err != nil {
+			g.Log().Line().Panic(err)
+		}
+	}
+
 }
 
 func init() {
--- packed/swagger.go
+++ packed/swagger.go
@@ -3,7 +3,7 @@ package packed
 import "github.com/gogf/gf/os/gres"
 
 func init() {
-	if err := gres.Add("H4sIAAAAAAAC/wrwZmYRYeBg4GBYIWwexIAERBg4GYrLE9PTU4v0obReVnF+XmgIKwPjn07uhJ5JB/0OOQi0vbc5uiOdiVVR7c6el2dnszunBLeVTS4r32y9ouXLM+WKSRlPkva8mCusr7wgP3HXWqdc1W9Xfj6fXV9Wlh7FzJBwjd/Q7XRVwUMW9TaXWTs3xJ/fmC3HkhHBH7H74cYY0aIlEfzLbp9SjLrP4et0coosS1ZKRc90wSJl9gtZsqV7D/st5579O/rE5A1VtXXxNqbX9T9/PfypeN+NM/casieWF+5T0Th0ZWMCx/H05bunrFcpnH9JWdX6k6jUbPkVVz9IxZlF+Rmc1dxyVHNW6wVPxiDPM6d6pfr+xptHKHDkr1iprKeizfi36ubEig3H37Is9vh7/kTf4xmzPa2+H/+UZ1rdsmYHk2nG1kliuU8n8GVW35vvuaKi+jr3uamdbeyiuvWn7rB92nutb3FnmXTCSQ7JKfkPfntrGaduXZGz+I3+StV4RgaG//8DvNk5qi2a7zkzMjAoMTMwwGKDASM22BGxAY8AkG5kNQHejEwizIjYRDYZFJswsK0RROKNW4RR2J0CAQIM/x37GBmwOIyVDSTPxMDE0MnAwHCGEcQDBAAA//+pHTbmawIAAA=="); err != nil {
+	if err := gres.Add("H4sIAAAAAAAC/wrwZmYRYeBg4GBwFDMPYkACIgycDMXlienpqUX6UFovqzg/LzSElYHxVh93Qs+kg36HHATa3tsc3ZHOpOvks6Py8mxz/rYTy3hSJ5eVb7Ze0fLlmXLFpIwnSXtezBXWV16Qn7hrrVOu6rcrP5/Pri8rS49iZkgwk0jsmVb68BCnn4xg5rNDcbbH11VKLEqMTVjbdrwo9MTCpNgZedrsGczKJpyqwdvkVy18aBjndqL3TfN7+dI9h8t2M8/+HX1i8oaq2rp4G9Pr+p+/Hv5UvO/GmXsN2RPLC/epaBy6sjGB43j68t0hC1Q+NlxSVrX+JCo1W/5EfgWvvbSu+oxnJpuOas5qveCpGOR55lSvVE9zvHmEAkf+ipXKeirajHurbk6s2HH8Lctij7/nT/Q9njHb0+r78U95ptUta3YwmWZsnSSW+3QCX2blvfmeKyqqr3Ofm9rJxi6qW3/qDtunvdf6FneWSSec5JCclv/gt7eWcerWFTmL3+ivVI1nZGD4/z/Am53D/zXrb2dGBgYlZgYGWGwwYMQGOyI24BEA0o2sJsCbkUmEGRGbyCaDYhMGtjWCSLxxizAKu1MgQIDhv2MfIwMWh7GygeSZGJgYOhkYGM4wgniAAAAA///Yr4RuawIAAA=="); err != nil {
 		panic("add binary content to resource manager failed: " + err.Error())
 	}
 }

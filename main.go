930601e5f5697c5a0ce96dab4cec25c1a9bb2bf3
117503445 <t117503445@gmail.com>
2021-03-02 20:37:24 +0800

    chore: text to lf
    1.2.2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // @title       goframe_learn API
-// @version     1.2.1
+// @version     1.2.2
 // @description `goframe_learn` 117503445 的 goframe 学习/模板项目 api
 // @schemes     http
 
--- script/preprocess.py
+++ script/preprocess.py
@@ -1,15 +1,29 @@
 import subprocess
+from pathlib import Path
+
+
+def run_shell_script(script: str):
+    lines = script.split('\n')
+    for line in lines:
+        subprocess.call(args=line, shell=True)
+
+
+def text_to_lf():
+    for path in Path('.').glob('**/*'):
+        print(path)
+        if str(path)[0] != '.' and not path.is_dir():
+            with open(path, 'r', encoding='utf-8')as f:
+                lines = f.readlines()
+                text = ''.join(lines)
+            with open(path, 'w', newline='\n', encoding='utf-8')as f:
+                f.write(text)
+
 
 def main():
     run_shell_script('''gf gen dao
 gf swagger --pack -y
 gofmt -l -s -w ./''')
-
-
-def run_shell_script(script:str):
-    lines = script.split('\n')
-    for line in lines:
-        subprocess.call(args=line, shell=True)
+    text_to_lf()
 
 
 if __name__ == '__main__':
